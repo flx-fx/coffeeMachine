7ab0e7dfc8b64d4bb4f4feda5b62cb873d53733c
flx-fx <felixfx@outlook.de>
2023-02-24 02:48:59 +0100

    Initial Commit

--- Topics/Secondary constructor/Frozen desserts/src/Main.kt
+++ Topics/Secondary constructor/Frozen desserts/src/Main.kt
@@ -1,1 +1,10 @@
-// write the IceCreamOrder class here
+class IceCreamOrder {
+    val price: Int
+
+    constructor(popsicles: Int) {
+        price = 7 * popsicles
+    }
+    constructor(scoops: Int, toppings: Int) {
+        price = 5 * scoops + 2 * toppings
+    }
+}
